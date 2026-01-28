043e4e12fa486b2e8f011eac99b4da33aa1da5b6
syugeeeeeeeeeei <tomo.syugei99@gmail.com>
2026-01-28 23:27:29 +0000

    up

--- services/upload_page/src/constants/config.ts
+++ services/upload_page/src/constants/config.ts
@@ -2,9 +2,9 @@
 export const CONFIG = {
     chainId: 'gwc',
     chainName: 'Cryptomeria Gateway',
-    rpcEndpoint: 'http://localhost:30007',
-    restEndpoint: 'http://localhost:30003',
-    faucetEndpoint: 'http://localhost:30045',
+    rpcEndpoint: 'http://127.0.0.1:30007',
+    restEndpoint: 'http://127.0.0.1:30003',
+    faucetEndpoint: 'http://127.0.0.1:30045',
     denom: 'uatom',    // 修正: ugwc -> uatom
     minDenom: 'ATOM',  // 修正: GWC -> ATOM
 };
