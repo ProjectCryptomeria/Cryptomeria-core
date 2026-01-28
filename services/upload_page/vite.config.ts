caeb6cc00f00e8b94021bc206c406e7cade8d108
syugeeeeeeeeeei <tomo.syugei99@gmail.com>
2026-01-28 16:04:13 +0000

    vite

--- services/upload_page/vite.config.ts
+++ services/upload_page/vite.config.ts
@@ -22,6 +22,10 @@ export default defineConfig({
             '@': path.resolve(__dirname, './src'),
         },
     },
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+    },
     build: {
         outDir: 'dist', // 出力先ディレクトリ
         emptyOutDir: true,
