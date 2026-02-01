fff95c104f8b0f2851fe812a2582f032a24b02a3
syugeeeeeeeeeei <tomo.syugei99@gmail.com>
2026-02-01 23:58:57 +0000

    1

--- ops/scripts/experiment/lib/config.ts
+++ ops/scripts/experiment/lib/config.ts
@@ -6,8 +6,8 @@ export const CONFIG = {
   GWC_RPC: "http://localhost:30007",
   GWC_API: "http://localhost:30003",
   RENDER_URL: "http://localhost:30003/render",
-  CHAIN_ID: "gwc-1",
-  DENOM: "ucrypt",
+  CHAIN_ID: "gwc",
+  DENOM: "uatom",
   BIN: {
     GWC: "/workspace/apps/gwc/dist/gwcd",
   },
